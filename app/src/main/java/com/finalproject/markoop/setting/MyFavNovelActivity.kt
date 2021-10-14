b60d74514a3d548f8377e10af98f8f5194182d1b
Dhiyaul Aulia <dhiyaul.aulia2003@gmail.com>
2021-10-14 13:45:43 +0700

    add my fav novel page, add publish page, add btn seconday, add arrow left, add publish illustration

---
+++ app/src/main/java/com/finalproject/markoop/setting/MyFavNovelActivity.kt
@@ -0,0 +1,12 @@
+package com.finalproject.markoop.setting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.finalproject.dean.markoop.R
+
+class MyFavNovelActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_fav_novel)
+    }
+}
