71fd44077680a44195333d5ec6ee804c6935146b
Dhiyaul Aulia <dhiyaul.aulia2003@gmail.com>
2021-10-15 20:16:14 +0700

    add detail novel act, change package name

---
+++ app/src/main/java/com/finalproject/markoop/DetailNovelActivity.kt
@@ -0,0 +1,12 @@
+package com.finalproject.markoop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.finalproject.dean.markoop.R
+
+class DetailNovelActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_novel)
+    }
+}
